0f7d14711f472e3d74a0d24a3293daefc731e3d6
HBJ <bomul0327@naver.com>
2017-11-29 17:29:45 +0900

    fix

---
+++ Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalManager : MonoBehaviour {
+
+	void Awake(){
+		Screen.SetResolution(Screen.height * 9 / 16, Screen.height, true);
+		Application.targetFrameRate = 60;
+	}
+	
+	void OnApplicationQuit()
+	{
+		
+	}
+
+	void OnApplicationPause(bool pauseStatus)
+	{
+		
+	}
+
+	void OnApplicationFocus(bool focusStatus)
+	{
+		
+	}
+}
--- Assets/Scripts/Play.cs
+++ Assets/Scripts/Play.cs
@@ -3,59 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 게임 플레이 관리
 public class Play : MonoBehaviour {
 
-    public GameObject red_circle;
-    public GameObject green_circle;
-    public GameObject blue_circle;
+    public int grid_size_x = 11;
+    public int grid_size_y = 11;
+    public float padding_x = 0.2f;
+    public float padding_y = 0.2f;
 
+    public Vector2 init_circle_pos = new Vector2(-2, -2);
+    public List<GameObject> circle_prefab_list;
+
+    // 모든 원들을 관리하는 게임오브젝트 배열
     GameObject[,] circles = new GameObject[11, 11];
 
     // Use this for initialization
     void Start () {
-        StartCoroutine("CreateDots");
+        StartCoroutine(CreateDots());
 	}
-    
-    IEnumerator CreateDots()
-    {
-        System.Random rand = new System.Random();
-        for (int y = -20; y <= 20; y += 4)
-        {
-            for (int x = -20; x <= 20; x += 4)
-            {
-                int type = rand.Next(3);
-                if (type == 0)
-                    circles[y / 4 + 5, x / 4 + 5] = Instantiate(red_circle, new Vector3(x / 10.0f, y / 10.0f, -1), Quaternion.identity);
-                else if (type == 1)
-                    circles[y / 4 + 5, x / 4 + 5] = Instantiate(green_circle, new Vector3(x / 10.0f, y / 10.0f, -1), Quaternion.identity);
-                else
-                    circles[y / 4 + 5, x / 4 + 5] = Instantiate(blue_circle, new Vector3(x / 10.0f, y / 10.0f, -1), Quaternion.identity);
-                object[] param = new object[2] { y / 4 + 5, x / 4 + 5 };
-                StartCoroutine("Bounce", param);
-                yield return new WaitForSeconds(0.02f);
-            }
-        }
-    }
-	
-    IEnumerator Bounce(object[] param)
-    {
-        int y = (int)param[0];
-        int x = (int)param[1];
-        for (float t = 0.0f; t <= 0.2f; t += 0.01f)
-        {
-            circles[y, x].transform.position = new Vector3((x - 5) * 4.0f / 10.0f, (y - 5) * 4.0f / 10.0f - 20.0f * t * (t - 0.2f), -1.0f);
-            yield return new WaitForSeconds(0.01f);
-        }
-        for (float t = 0.0f; t <= 0.2f; t += 0.01f)
-        {
-            circles[y, x].transform.position = new Vector3((x - 5) * 4.0f / 10.0f, (y - 5) * 4.0f / 10.0f - 10.0f * t * (t - 0.2f), -1.0f);
-            yield return new WaitForSeconds(0.01f);
-        }
-        circles[y, x].transform.position = new Vector3((x - 5) * 4.0f / 10.0f, (y - 5) * 4.0f / 10.0f, -1.0f);
-    }
 
-	// Update is called once per frame
-	void Update () {
+    IEnumerator CreateDots(){
+        Vector2 curr_pos = init_circle_pos;
+        for(int y = 0; y < grid_size_y; ++y){
+            for(int x = 0; x < grid_size_x; ++x){
+                circles[y, x] = Instantiate(circle_prefab_list[UnityEngine.Random.Range(0, circle_prefab_list.Count)], curr_pos, Quaternion.identity);
+                StartCoroutine(Bounce(circles[y, x].transform));
+                yield return new WaitForSeconds(0.02f);
+                curr_pos += new Vector2(padding_x, 0);
+            }
+            curr_pos = new Vector2(init_circle_pos.x, curr_pos.y + padding_y);
+        }
+    }
+    
+    IEnumerator Bounce(Transform tr){
+        Vector2 origin_pos = tr.position;
+        for(float t = 0.0f; t < 0.2f; t += 0.01f){
+            tr.position = new Vector2(origin_pos.x, origin_pos.y - 20f * t * (t - 0.2f));
+            yield return new WaitForSeconds(0.01f);
+        }
+        for(float t = 0.0f; t < 0.2f; t += 0.01f){
+            tr.position = new Vector2(origin_pos.x, origin_pos.y - 10.0f * t * (t - 0.2f));
+            yield return new WaitForSeconds(0.01f);
+        }
+        tr.position = origin_pos;
+        yield return null;
         
-	}
+    }
 }
